af498fbbf4047dccd1b077d1a1a0a85d1dd01735
Youssef Youssef <youssefyoussef28@gmail.com>
2023-03-03 16:10:34 +0200

    Next Lesson

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBDglr853uciMYQ4Dyizrba0Nv8b6ZaheQ",
   authDomain: "fir-9-dojo-65e47.firebaseapp.com",
@@ -6,5 +9,26 @@ const firebaseConfig = {
   messagingSenderId: "429081902893",
   appId: "1:429081902893:web:9375328ce78e02cb6e5da4",
 };
+// Init firebase app -  Connecting this frontend to the firebase backend
+initializeApp(firebaseConfig);
 
+// init services
+const db = getFirestore();
 
+// collection ref
+const colRef = collection(db, "books");
+
+// get collection data
+getDocs(colRef)
+  .then((snapshot) => {
+    let books = [];
+    snapshot.docs.forEach((doc) => {
+      books.push({ ...doc.data(), id: doc.id });
+    });
+    console.log(books);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+  
