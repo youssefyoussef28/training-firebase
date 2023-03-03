1e38c15f39ab4411eced82039f2923bd1519a807
Youssef Youssef <youssefyoussef28@gmail.com>
2023-03-03 19:24:14 +0200

    Login/Logout/SignUp with Firebase Auth

--- src/index.js
+++ src/index.js
@@ -9,7 +9,18 @@ import {
   onSnapshot,
   query,
   where,
+  orderBy,
+  serverTimestamp,
+  getDoc,
+  updateDoc,
 } from "firebase/firestore";
+
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signOut,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBDglr853uciMYQ4Dyizrba0Nv8b6ZaheQ",
@@ -19,25 +30,24 @@ const firebaseConfig = {
   messagingSenderId: "429081902893",
   appId: "1:429081902893:web:9375328ce78e02cb6e5da4",
 };
+
 // Init firebase app -  Connecting this frontend to the firebase backend
 initializeApp(firebaseConfig);
 
 // init services
 const db = getFirestore();
+const auth = getAuth();
 
 // collection ref
 const colRef = collection(db, "books");
 
 // queries
-
-const q = query(colRef, where("author", "==", "patrick rothfuss"));
+const q = query(colRef, orderBy("createdAt"));
 
 // Gets once collection data
-
 // getDocs(colRef).then((snapshot) => {});
 
 // Runs once initially & runs every-time Change - real-time
-
 onSnapshot(q, (snapshot) => {
   let books = [];
   snapshot.docs.forEach((doc) => {
@@ -53,6 +63,7 @@ addBookForm.addEventListener("submit", (e) => {
   addDoc(colRef, {
     title: addBookForm.title.value,
     author: addBookForm.author.value,
+    createdAt: serverTimestamp(),
   }).then(() => {
     addBookForm.reset();
   });
@@ -67,4 +78,66 @@ deleteBookForm.addEventListener("submit", (e) => {
   deleteDoc(docRef).then(() => {
     deleteBookForm.reset();
   });
+});
+
+// get a single document
+const docRef = doc(db, "books", "XcKH9hkfSHufpLPyHIR7");
+onSnapshot(docRef, (doc) => {
+  console.log(doc.data(), doc.id);
+});
+
+// update a single document
+const updateForm = document.querySelector(".update");
+updateForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const docRef = doc(db, "books", updateForm.id.value);
+  updateDoc(docRef, {
+    title: "updated title",
+  }).then(() => {
+    updateForm.reset();
+  });
+});
+
+// signing users up
+const signupForm = document.querySelector(".signup");
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const email = signupForm.email.value;
+  const password = signupForm.password.value;
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((cred) => {
+      console.log("User Created:", cred.user);
+      signupForm.reset();
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
+
+// logging in and out
+const logoutButton = document.querySelector(".logout");
+logoutButton.addEventListener("click", () => {
+  signOut(auth)
+    .then(() => {
+      console.log("User Signed Out");
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
+
+const loginForm = document.querySelector(".login");
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = loginForm.email.value;
+  const password = loginForm.password.value;
+  signInWithEmailAndPassword(auth, email, password)
+    .then((cred) => {
+      console.log("User Logged in", cred.user);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 });
