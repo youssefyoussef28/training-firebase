747dea7f20b6899b021c3211c5b9267c4d2b3b18
Youssef Youssef <youssefyoussef28@gmail.com>
2023-03-03 15:30:16 +0200

    Lesson 2

---
+++ dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("const firebaseConfig = {\r\n  apiKey: \"AIzaSyBDglr853uciMYQ4Dyizrba0Nv8b6ZaheQ\",\r\n  authDomain: \"fir-9-dojo-65e47.firebaseapp.com\",\r\n  projectId: \"fir-9-dojo-65e47\",\r\n  storageBucket: \"fir-9-dojo-65e47.appspot.com\",\r\n  messagingSenderId: \"429081902893\",\r\n  appId: \"1:429081902893:web:9375328ce78e02cb6e5da4\",\r\n};\r\n\r\n\r\n\n\n//# sourceURL=webpack://training-firebase/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBDglr853uciMYQ4Dyizrba0Nv8b6ZaheQ",
+  authDomain: "fir-9-dojo-65e47.firebaseapp.com",
+  projectId: "fir-9-dojo-65e47",
+  storageBucket: "fir-9-dojo-65e47.appspot.com",
+  messagingSenderId: "429081902893",
+  appId: "1:429081902893:web:9375328ce78e02cb6e5da4",
+};
+
+
